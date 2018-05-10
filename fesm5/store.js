628ab765f79639020c9710589f25be1dd67f7c4a
Brandon <robertsbt@gmail.com>
2018-05-10 12:38:02 +0000

    b329aa1 docs(Effects): improve documentation for non-dispatching effects (#1050)

--- fesm5/store.js
+++ fesm5/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx v6.0.0-beta.1+36.sha-290cbf7
+ * @license NgRx v6.0.0-beta.1+37.sha-b329aa1
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
