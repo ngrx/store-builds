3494f87b330aa76ede5b530c4fc914e786ca7bdf
GitHub <noreply@github.com>
2018-05-11 15:31:36 +0000

    92c13fd Revert "build: Add script to copy schematics-core to supported packages (#1052)" (#1055)

--- bundles/store.umd.js
+++ bundles/store.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx v6.0.0-beta.1+39.sha-5b4f067
+ * @license NgRx v6.0.0-beta.1+41.sha-92c13fd
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
