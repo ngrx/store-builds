90f4eb1aa6e8ec3077bbb4524b438b27911e44ae
Brandon <robertsbt@gmail.com>
2018-05-11 01:20:32 +0000

    5b4f067 feat(Store): Add support ng update (#1032)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx v6.0.0-beta.1+37.sha-b329aa1
+ * @license NgRx v6.0.0-beta.1+39.sha-5b4f067
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
