7d7a4f640a8048e00596600f9f18946d44805417
GitHub <noreply@github.com>
2018-05-12 05:28:09 +0000

    253411e chore: Update migrations to 6.0.0-beta.3 (#1058)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx v6.0.0-beta.2+1.sha-355b12f
+ * @license NgRx v6.0.0-beta.2+2.sha-253411e
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
