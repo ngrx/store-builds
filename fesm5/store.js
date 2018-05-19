10cd85fba1735288ba40fcd2c1d045052d406be5
Brandon <robertsbt@gmail.com>
2018-05-19 02:56:55 +0000

    440b6cf docs(Schematics): edit README to match ng cli docs (#1078)

--- fesm5/store.js
+++ fesm5/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx v6.0.0-beta.3+4.sha-599cfb6
+ * @license NgRx v6.0.0-beta.3+7.sha-440b6cf
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
