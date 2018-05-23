0147f48a9861c16064f5ed146d2cbe1fe229d25b
Brandon <robertsbt@gmail.com>
2018-05-23 00:59:00 +0000

    a478c0d refactor(SchematicsCore): remove npm install task during ng update (#1080)

--- fesm2015/store.js
+++ fesm2015/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx v6.0.0-beta.3+7.sha-440b6cf
+ * @license NgRx v6.0.0-beta.3+8.sha-a478c0d
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
--- schematics-core/index.d.ts
+++ schematics-core/index.d.ts
@@ -2,7 +2,7 @@
 import { dasherize, decamelize, camelize, classify, underscore, group, capitalize, featurePath } from './utility/strings';
 export { findNodes, getSourceNodes, getDecoratorMetadata, getContentOfKeyLiteral, insertAfterLastOccurrence, addBootstrapToModule, addDeclarationToModule, addExportToModule, addImportToModule, addProviderToModule } from './utility/ast-utils';
 export { Host, Change, NoopChange, InsertChange, RemoveChange, ReplaceChange } from './utility/change';
-export { AppConfig, CliConfig, getAppFromConfig, getConfig, getWorkspace, getWorkspacePath } from './utility/config';
+export { AppConfig, getWorkspace, getWorkspacePath } from './utility/config';
 export { findModule, findModuleFromOptions, buildRelativePath, ModuleOptions } from './utility/find-module';
 export { addReducerToState, addReducerToStateInferface, addReducerImportToNgModule, addReducerToActionReducerMap, omit } from './utility/ngrx-utils';
 export { getProjectPath } from './utility/project';
