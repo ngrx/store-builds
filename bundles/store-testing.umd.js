14b000991acc503a47a9764a2135cce87bad5746
Brandon <robertsbt@gmail.com>
2018-11-12 20:00:29 +0000

    1d23fe8 docs: add NgRx resources page (#1418)

--- bundles/store-testing.umd.js
+++ bundles/store-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 7.0.0-beta.0+7.sha-58644d2
+ * @license NgRx 7.0.0-beta.0+8.sha-1d23fe8
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
