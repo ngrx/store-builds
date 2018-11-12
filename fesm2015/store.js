9da9ceeaefd9fd217b284854140900eed5acf011
GitHub <noreply@github.com>
2018-11-12 17:33:01 +0000

    58644d2 docs: add store tutorial example and tweak build to support examples (#1417)

--- fesm2015/store.js
+++ fesm2015/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 7.0.0-beta.0+6.sha-958f3b6
+ * @license NgRx 7.0.0-beta.0+7.sha-58644d2
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
