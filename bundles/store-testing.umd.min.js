c07b1ffc9dcfa2fe3073b648b468c06329cca218
Brandon <robertsbt@gmail.com>
2019-02-18 04:59:58 +0000

    8532fb1 test: update watch:tests script to work with Windows (#1560)

--- bundles/store-testing.umd.min.js
+++ bundles/store-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 7.2.0.with-local-changes
+ * @license NgRx 7.2.0+1.sha-8532fb1.with-local-changes
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
