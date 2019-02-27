2724d5cbbfe238b9d92cff23ce02e728346e3110
Brandon <robertsbt@gmail.com>
2019-02-27 01:19:26 +0000

    e86c5f6 fix(store): deprecate signature for selector with only a projector (#1580)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 7.2.0+8.sha-5fbcb3c.with-local-changes
+ * @license NgRx 7.2.0+9.sha-e86c5f6.with-local-changes
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
