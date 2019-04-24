93b55e32c09cc38905a79d6c47546c5e42bb640e
Brandon <robertsbt@gmail.com>
2019-04-24 17:45:29 +0000

    46678f2 docs: add explanation and suggested fix for string enforcement of MemoizedSelector type (#1772)

--- fesm5/store.js
+++ fesm5/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+1.sha-5f23d9d
+ * @license NgRx 8.0.0-beta.1+2.sha-46678f2
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
