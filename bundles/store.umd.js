fb407a2c6b2c05c33f9398c8729746ca2ccf2d29
Brandon <robertsbt@gmail.com>
2019-04-25 02:27:22 +0000

    63e4926 feat(store): expand createReducer type signature to support up to ten action creators (#1803)

--- bundles/store.umd.js
+++ bundles/store.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+3.sha-ecda5f7
+ * @license NgRx 8.0.0-beta.1+4.sha-63e4926
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
