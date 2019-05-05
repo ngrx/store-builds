726831dc0a1ce0ef9d7324251910414276a6d696
Brandon <robertsbt@gmail.com>
2019-05-05 18:50:42 +0000

    e1fae9e refactor(data): rename @params to @param to remove jsdoc warning (#1817)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+9.sha-18f382b
+ * @license NgRx 8.0.0-beta.1+10.sha-e1fae9e
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
