299db3e60ea6c42d0ca1d78c3f8d60f4603bc4d5
Brandon <robertsbt@gmail.com>
2019-05-05 18:24:19 +0000

    8755d07 docs: add section to Store testing guide on using mock selectors (#1797)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+5.sha-00c00e0
+ * @license NgRx 8.0.0-beta.1+6.sha-8755d07
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
