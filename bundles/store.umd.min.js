ebd9ef689c1ee3ccf46f06820672eac62a89b7e7
Brandon <robertsbt@gmail.com>
2019-05-05 18:44:27 +0000

    18f382b docs: remove undeclared close method call in data example (#1815)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+8.sha-57e363f
+ * @license NgRx 8.0.0-beta.1+9.sha-18f382b
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
