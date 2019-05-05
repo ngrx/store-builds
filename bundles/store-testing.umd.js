cff95833b0b77fefe41c2d98b6cfde5ee01c1d1c
Brandon <robertsbt@gmail.com>
2019-05-05 18:40:31 +0000

    57e363f chore: don't copy build files on copySchematicsCore (#1809)

--- bundles/store-testing.umd.js
+++ bundles/store-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+6.sha-8755d07
+ * @license NgRx 8.0.0-beta.1+8.sha-57e363f
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
