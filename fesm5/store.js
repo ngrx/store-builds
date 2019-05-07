5fdff714203aa9431706ae11fe8c9d9452970c07
Brandon <robertsbt@gmail.com>
2019-05-07 17:31:32 +0000

    1fbd59c fix(example): resolve circular dependency (#1833)

--- fesm5/store.js
+++ fesm5/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+13.sha-e836719
+ * @license NgRx 8.0.0-beta.1+14.sha-1fbd59c
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
