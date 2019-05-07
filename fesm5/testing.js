4bc83bb0ec0068054a5214009453392dc7b53025
Brandon <robertsbt@gmail.com>
2019-05-07 18:55:54 +0000

    709c2cb docs: update team page for members, fix styles (#1832)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+14.sha-1fbd59c
+ * @license NgRx 8.0.0-beta.1+15.sha-709c2cb
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
