b9dbbd6c4731cc2ab959f51a9a8717266c260806
Brandon <robertsbt@gmail.com>
2019-05-09 15:07:16 +0000

    a56ddfa docs: update section titles in sidenav to use package names (#1844)

--- bundles/store-testing.umd.js
+++ bundles/store-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+18.sha-b07ae4e
+ * @license NgRx 8.0.0-beta.1+19.sha-a56ddfa
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
