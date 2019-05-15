1bf4080e0b3fc35a9a25f4c8d7ff085482847e10
Brandon <robertsbt@gmail.com>
2019-05-15 19:21:12 +0000

    c19f820 docs: add iJS London talk and slide link (#1859)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+24.sha-1ff986f
+ * @license NgRx 8.0.0-beta.1+25.sha-c19f820
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
