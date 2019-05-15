ddba2f9167e0e4bdf1192bf27b276532698c4ee3
Brandon <robertsbt@gmail.com>
2019-05-15 18:06:25 +0000

    1ff986f feat(effects): add mapToAction operator (#1822)

--- fesm5/store.js
+++ fesm5/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+23.sha-873bc36
+ * @license NgRx 8.0.0-beta.1+24.sha-1ff986f
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
