0358f3e6c9893c8cde1f6d1182d2be909d78ba27
Brandon <robertsbt@gmail.com>
2019-05-15 19:40:16 +0000

    ba52904 docs(effects): add missing imports in EffectsModule example (#1862)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+25.sha-c19f820
+ * @license NgRx 8.0.0-beta.1+26.sha-ba52904
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
