333027cd6e51b73e7d65bc8258ac000a9cc85312
Brandon <robertsbt@gmail.com>
2019-05-15 02:31:30 +0000

    873bc36 docs: remove deprecated Buffer function (#1858)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.1+22.sha-92a6b20
+ * @license NgRx 8.0.0-beta.1+23.sha-873bc36
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
