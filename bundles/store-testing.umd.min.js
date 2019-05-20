06c3a1c1b219677d422c71d480d1bc38dc382e44
Brandon <robertsbt@gmail.com>
2019-05-20 20:23:13 +0000

    d874cfc feat(router-store): add routerState config option (#1847)

--- bundles/store-testing.umd.min.js
+++ bundles/store-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.2+3.sha-29c426b
+ * @license NgRx 8.0.0-beta.2+4.sha-d874cfc
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
