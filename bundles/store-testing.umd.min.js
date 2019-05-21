f64cee3d8caad0e5585cc62299e6911ef98c30ef
Brandon <robertsbt@gmail.com>
2019-05-21 14:22:25 +0000

    3454e70 feat(example): add examples of effects not based on the Actions stream (#1845)

--- bundles/store-testing.umd.min.js
+++ bundles/store-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.2+4.sha-d874cfc
+ * @license NgRx 8.0.0-beta.2+5.sha-3454e70
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
