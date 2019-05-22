a9525b52fab7fa883f9cef3f362195b4b4c0d6e0
Brandon <robertsbt@gmail.com>
2019-05-22 22:28:47 +0000

    478b225 docs: improve responsiveness on the home page for small screens (#1870)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.2+8.sha-23c7c75
+ * @license NgRx 8.0.0-beta.2+9.sha-478b225
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
