0de02e2af9e8d3b868af6ef72858cff91031b7a6
Brandon <robertsbt@gmail.com>
2019-05-24 01:43:57 +0000

    fd0a63c docs(effects): correct usage of pipe with mapToAction example (#1886)

--- bundles/store-testing.umd.min.js
+++ bundles/store-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.2+10.sha-71137e5
+ * @license NgRx 8.0.0-beta.2+11.sha-fd0a63c
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
