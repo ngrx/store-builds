7a9839d7b3355c20685958cfb62685eef73ffbd6
Brandon <robertsbt@gmail.com>
2019-05-26 21:02:28 +0000

    3fdd78d ci(example): re-enable E2E tests in CI (#1889)

--- fesm2015/store.js
+++ fesm2015/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.2+11.sha-fd0a63c
+ * @license NgRx 8.0.0-beta.2+12.sha-3fdd78d
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
