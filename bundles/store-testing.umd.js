10fe75fc55828ab5a20ad1cbac5239c44130ba84
GitHub <noreply@github.com>
2019-05-29 17:58:36 +0000

    ecefaa6 chore: update dependencies to Angular version 8.0.0 (#1898)

--- bundles/store-testing.umd.js
+++ bundles/store-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.2+15.sha-2110e91
+ * @license NgRx 8.0.0-beta.2+16.sha-ecefaa6
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
