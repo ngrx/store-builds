829aea0916f39481de5641fcd580dbd3915331fd
Brandon <robertsbt@gmail.com>
2019-05-29 03:03:09 +0000

    2110e91 docs: add return type changes and example of resubscribe on errors (#1893)

--- fesm5/store.js
+++ fesm5/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.2+14.sha-c59c211
+ * @license NgRx 8.0.0-beta.2+15.sha-2110e91
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
