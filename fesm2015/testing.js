c3a669e87b2b38704c62242555cb9d4dd77737ab
Brandon <robertsbt@gmail.com>
2019-05-29 23:11:19 +0000

    21c67cc feat(router-store): add selectors for router state (#1874)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-beta.2+17.sha-d5e3c0c
+ * @license NgRx 8.0.0-beta.2+18.sha-21c67cc
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
