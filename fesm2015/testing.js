0bf021a09c81fe1cc553cc41ace88073194f51be
Brandon <robertsbt@gmail.com>
2019-06-04 15:02:25 +0000

    c3b481b docs: add contributing page (#1911)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-rc.0+3.sha-4146650
+ * @license NgRx 8.0.0-rc.0+4.sha-c3b481b
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
