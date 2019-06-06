dc0d4577086deb63f2dbe36ee89ecb4c2eb81fea
Brandon <robertsbt@gmail.com>
2019-06-06 20:37:59 +0000

    36f14bd refactor(store): rename mapToAction operator to act  (#1927)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.0.0-rc.1+4.sha-dec4bfa
+ * @license NgRx 8.0.0-rc.1+5.sha-36f14bd
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
