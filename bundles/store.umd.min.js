6e539f99399bdeaa434aea32a25d0c573218ed5b
Brandon <robertsbt@gmail.com>
2019-10-24 12:26:42 +0000

    1b4ba1a feat(store): add On interface to public api (#2157)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 8.4.0+10.sha-0d59783
+ * @license NgRx 8.4.0+11.sha-1b4ba1a
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
--- src/index.d.ts
+++ src/index.d.ts
@@ -9,4 +9,4 @@ export { createSelector, createSelectorFactory, createFeatureSelector, defaultMe
 export { State, StateObservable, reduceState } from './state';
 export { INITIAL_STATE, REDUCER_FACTORY, INITIAL_REDUCERS, STORE_FEATURES, META_REDUCERS, FEATURE_REDUCERS, USER_PROVIDED_META_REDUCERS, USER_RUNTIME_CHECKS, } from './tokens';
 export { StoreModule, StoreRootModule, StoreFeatureModule, RootStoreConfig, StoreConfig, } from './store_module';
-export { on, createReducer } from './reducer_creator';
+export { On, on, createReducer } from './reducer_creator';
