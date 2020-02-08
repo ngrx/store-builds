5ddeb0e3fe1dc65c29daeab8e28f76612afbeb2d
GitHub <noreply@github.com>
2020-02-08 17:16:36 +0000

    637b2c7 fix(data): make undoMany remove tracking changes in changeState (#2346) (#2352)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+4.sha-f09df95
+ * @license NgRx 9.0.0-beta.0+5.sha-637b2c7
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
