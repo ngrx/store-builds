e085620b28f63166fbec36dd151e107fd140ef4b
GitHub <noreply@github.com>
2020-02-09 10:29:01 +0000

    7b3aea3 chore: update examples to Angular v9 and NgRx v9 latest (#2360)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+5.sha-637b2c7
+ * @license NgRx 9.0.0-beta.0+6.sha-7b3aea3
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
