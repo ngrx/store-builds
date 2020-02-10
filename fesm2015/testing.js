6137e2e1a6f9b1f721fe7e613da750863fa1f16b
GitHub <noreply@github.com>
2020-02-10 22:00:56 +0000

    234ce84 fix(data): EntityDataModuleWithoutEffect ModuleWithProviders (#2366)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+6.sha-7b3aea3
+ * @license NgRx 9.0.0-beta.0+7.sha-234ce84
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
