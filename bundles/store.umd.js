3c779d15ad30bd2d164aeb866b5ab0baaf1d7ec3
GitHub <noreply@github.com>
2020-02-13 15:36:43 +0000

    b42f7da docs: update entry for ngrx-ducks (#2379)

--- bundles/store.umd.js
+++ bundles/store.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+10.sha-88124a7
+ * @license NgRx 9.0.0-beta.0+11.sha-b42f7da
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
