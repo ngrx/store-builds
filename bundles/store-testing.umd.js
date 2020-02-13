c7cdfb53850df11d04833f2295282e19df46faa5
GitHub <noreply@github.com>
2020-02-13 14:26:23 +0000

    c305086 docs: update AngularInDepth NgRx resource link (#2378)

--- bundles/store-testing.umd.js
+++ bundles/store-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+8.sha-4cae255
+ * @license NgRx 9.0.0-beta.0+9.sha-c305086
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
