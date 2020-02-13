4c4d1cb25ffaeb60f804febdab0be11813e6a784
GitHub <noreply@github.com>
2020-02-13 15:01:43 +0000

    88124a7 feat(effects): limit retries to 10 by default (#2376)

--- fesm2015/store.js
+++ fesm2015/store.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+9.sha-c305086
+ * @license NgRx 9.0.0-beta.0+10.sha-88124a7
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
