02b16844c0428ce7891799fef76e3f49e2091dd2
GitHub <noreply@github.com>
2020-02-15 20:24:41 +0000

    943a2bb docs(data): correct typo in faq (#2385)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+12.sha-827f336
+ * @license NgRx 9.0.0-beta.0+13.sha-943a2bb
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
