648649351ed38f2dd959938b43e98fe6c38c5f01
GitHub <noreply@github.com>
2020-02-17 18:07:05 +0000

    5e84b37 refactor(example): use barrel import for GoogleBooksService (#2375)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+16.sha-bd86a99
+ * @license NgRx 9.0.0-beta.0+17.sha-5e84b37
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
