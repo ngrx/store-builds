be577102c43f2ea937c9ace3f3a20272f5da6a4b
GitHub <noreply@github.com>
2020-02-18 14:07:59 +0000

    521671f chore: update docs app to Angular 9 libraries (#2391)

--- bundles/store.umd.min.js
+++ bundles/store.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+21.sha-b3a49c1
+ * @license NgRx 9.0.0-beta.0+22.sha-521671f
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
