49cc926a8fafb783fb2750e8ec19099c4e6ae805
GitHub <noreply@github.com>
2020-02-18 14:03:52 +0000

    b3a49c1 fix(data): make mergeServerUpserts change state immutably (#2374) (#2389)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+20.sha-daf1e64
+ * @license NgRx 9.0.0-beta.0+21.sha-b3a49c1
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
