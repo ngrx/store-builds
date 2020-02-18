c6eee2389f0782675a986acb39da6e6d3f90c061
GitHub <noreply@github.com>
2020-02-18 10:52:02 +0000

    188a765 docs: fix typo in NgRx Data FAQs (#2393)

--- bundles/store.umd.js
+++ bundles/store.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.0+18.sha-ee2c114
+ * @license NgRx 9.0.0-beta.0+19.sha-188a765
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
