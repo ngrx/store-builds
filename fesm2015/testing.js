2e12ea330f82b934400a15bb312f51fb1528e455
GitHub <noreply@github.com>
2020-02-23 17:25:33 +0000

    d7ab532 docs(data): pluralizzer spelled wrong (#2407)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.1+3.sha-79c830c
+ * @license NgRx 9.0.0-beta.1+4.sha-d7ab532
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
