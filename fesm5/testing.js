e2ff73739a3b5bdbe038457e8beee5e7b421e490
GitHub <noreply@github.com>
2020-02-24 07:17:57 +0000

    23574ae docs(data): fix typo (#2409)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.1+5.sha-561610e
+ * @license NgRx 9.0.0-beta.1+6.sha-23574ae
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
