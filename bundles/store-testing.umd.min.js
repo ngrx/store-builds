d482b33bb3766db40c7ed40856224dab45ae7c26
GitHub <noreply@github.com>
2020-02-24 13:45:58 +0000

    37f91db feat(store-devtools): add INITIAL_OPTIONS to public API (#2405)

--- bundles/store-testing.umd.min.js
+++ bundles/store-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.1+6.sha-23574ae
+ * @license NgRx 9.0.0-beta.1+7.sha-37f91db
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
