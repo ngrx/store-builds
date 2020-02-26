a803c082f14687afdfdb16aee3f727eada119a8d
GitHub <noreply@github.com>
2020-02-26 09:51:07 +0000

    c9ebb06 fix(example): fix a typo selectShowSidenav (#2414)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.2+1.sha-a1a282d
+ * @license NgRx 9.0.0-beta.2+2.sha-c9ebb06
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
