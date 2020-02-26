32af6ac83458f5d03685be4fdac6ec27b096e968
GitHub <noreply@github.com>
2020-02-26 09:58:37 +0000

    711ba0e fix(data): correct AppEntityServices example in ngrx data doc page (#2413)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.2+2.sha-c9ebb06
+ * @license NgRx 9.0.0-beta.2+3.sha-711ba0e
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
