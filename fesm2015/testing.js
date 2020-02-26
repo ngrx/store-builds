19a557f3836e34d3df30c57561d897cf3a8ceaff
GitHub <noreply@github.com>
2020-02-26 02:34:04 +0000

    a1a282d test(data): add entity action guard tests (#2390)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-beta.1+9.sha-67af181
+ * @license NgRx 9.0.0-beta.2+1.sha-a1a282d
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
