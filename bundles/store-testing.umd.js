b1eaa794f133c3aa29e87bcc532f9886ad60de16
GitHub <noreply@github.com>
2020-03-05 03:30:41 +0000

    fba0dda chore: switch to Jest for unit tests (#2422)

--- bundles/store-testing.umd.js
+++ bundles/store-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license NgRx 9.0.0-rc.0+2.sha-0d6b6af
+ * @license NgRx 9.0.0-rc.0+3.sha-fba0dda
  * (c) 2015-2018 Brandon Roberts, Mike Ryan, Rob Wormald, Victor Savkin
  * License: MIT
  */
