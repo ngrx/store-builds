64eee581e227b4a8ac67a59da4934a09d31c9745
Brandon Roberts <robertsbt@gmail.com>
2020-06-25 16:51:47 +0000

    a7b0841a chore: release 10.0.0-beta.0

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
 exports.platformVersion = void 0;
-exports.platformVersion = '^9.0.0';
+exports.platformVersion = '^10.0.0-beta.0';
 //# sourceMappingURL=libs-version.js.map
