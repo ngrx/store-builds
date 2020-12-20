793ec4f10e14ae003fef4bac0c8d71829ca6d8eb
GitHub <noreply@github.com>
2020-12-20 22:49:48 +0000

    14328916 docs: add version 11 migration guide (#2792)

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
 exports.platformVersion = void 0;
-exports.platformVersion = '^10.0.1';
+exports.platformVersion = '^11.0.0-beta.0';
 //# sourceMappingURL=libs-version.js.map
