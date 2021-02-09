0f9048fda0271347aff65952082121d0cc874f63
Brandon Roberts <robertsbt@gmail.com>
2021-02-09 19:28:59 +0000

    01f9bd9e chore: release 11.0.0-rc.0

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
 exports.platformVersion = void 0;
-exports.platformVersion = '^11.0.0-beta.2';
+exports.platformVersion = '^11.0.0-rc.0';
 //# sourceMappingURL=libs-version.js.map
