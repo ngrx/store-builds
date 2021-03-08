b7e3d174be3175f20dcde2af8d78be99038df9e2
GitHub <noreply@github.com>
2021-03-08 23:33:33 +0000

    9b234033 fix(store): allow default parameters in function action (#2954)

--- src/action_creator.d.ts
+++ src/action_creator.d.ts
@@ -1,7 +1,7 @@
 import { Creator, ActionCreator, TypedAction, FunctionWithParametersType, NotAllowedCheck, ActionCreatorProps } from './models';
 export declare function createAction<T extends string>(type: T): ActionCreator<T, () => TypedAction<T>>;
 export declare function createAction<T extends string, P extends object>(type: T, config: ActionCreatorProps<P> & NotAllowedCheck<P>): ActionCreator<T, (props: P & NotAllowedCheck<P>) => P & TypedAction<T>>;
-export declare function createAction<T extends string, P extends any[], R extends object>(type: T, creator: Creator<P, R> & NotAllowedCheck<R>): FunctionWithParametersType<P, R & TypedAction<T>> & TypedAction<T>;
+export declare function createAction<T extends string, P extends any[], R extends object>(type: T, creator: Creator<P, R & NotAllowedCheck<R>>): FunctionWithParametersType<P, R & TypedAction<T>> & TypedAction<T>;
 export declare function props<P extends object>(): ActionCreatorProps<P>;
 export declare function union<C extends {
     [key: string]: ActionCreator<string, Creator>;
