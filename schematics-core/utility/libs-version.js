03f27bbd6130f788ee374ae5100d3c4d4b317b3a
Brandon <robertsbt@gmail.com>
2021-06-10 14:46:50 +0000

    b3e2b103 chore: release 12.1.0

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
 exports.platformVersion = void 0;
-exports.platformVersion = '^12.0.0';
+exports.platformVersion = '^12.1.0';
 //# sourceMappingURL=libs-version.js.map
