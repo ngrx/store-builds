552bb509c5258fd55aaf6505b7edf01f867a070d
GitHub <noreply@github.com>
2021-07-28 21:34:56 +0000

    23c846bd fix(component): capture errors from observable when using `ngrxPush` pipe and `ngrxLet` directive

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
 exports.platformVersion = void 0;
-exports.platformVersion = '^12.2.0';
+exports.platformVersion = '^12.3.0';
 //# sourceMappingURL=libs-version.js.map
