84c9746dcccca9bc79876ae540766f644597f925
GitHub <noreply@github.com>
2021-10-26 14:05:37 +0000

    9b65db34 docs(store): add missing types to walkthrough example (#3206)

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
 exports.platformVersion = void 0;
-exports.platformVersion = '^12.5.0';
+exports.platformVersion = '^12.5.1';
 //# sourceMappingURL=libs-version.js.map
