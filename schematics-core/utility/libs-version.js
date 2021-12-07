a3de8da60bfe9c1dc743f6846b9debc525d9d7fd
Brandon Roberts <robertsbt@gmail.com>
2021-12-07 13:47:41 +0000

    d3aa7d0b chore: update changelog to 13.0.2

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
 exports.platformVersion = void 0;
-exports.platformVersion = '^13.0.1';
+exports.platformVersion = '^13.0.2';
 //# sourceMappingURL=libs-version.js.map
