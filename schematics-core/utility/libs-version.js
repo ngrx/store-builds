c0738ee848d01939ed669bfdf0fd9345a86ea1f9
Brandon Roberts <robertsbt@gmail.com>
2023-10-03 18:10:42 +0000

    52e7dbd8 chore: release 16.3.0

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.platformVersion = void 0;
-exports.platformVersion = '^16.2.0';
+exports.platformVersion = '^16.3.0';
 //# sourceMappingURL=libs-version.js.map
