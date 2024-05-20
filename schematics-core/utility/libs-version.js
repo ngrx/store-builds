6a6d70c59ce8e8a299a13c3e54e1951b937236f5
Brandon Roberts <robertsbt@gmail.com>
2024-05-20 20:47:24 +0000

    98eb9dbd chore: release 18.0.0-beta.1

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.platformVersion = void 0;
-exports.platformVersion = '^17.2.0';
+exports.platformVersion = '^18.0.0-beta.1';
 //# sourceMappingURL=libs-version.js.map
