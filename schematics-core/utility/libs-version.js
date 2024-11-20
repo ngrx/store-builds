18d3184a946c6bcc48b7aaa8cd36d240dda1fbda
Brandon Roberts <robertsbt@gmail.com>
2024-11-20 21:29:49 +0000

    b57e7071 chore: typescript-eslint left-overs

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.platformVersion = void 0;
-exports.platformVersion = '^18.1.1';
+exports.platformVersion = '^19.0.0-beta.0';
 //# sourceMappingURL=libs-version.js.map
