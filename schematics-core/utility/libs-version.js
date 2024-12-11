c9031cbbd213b655ae32475b746e735ba7616d69
Brandon Roberts <robertsbt@gmail.com>
2024-12-11 00:12:58 +0000

    e8fe9088 chore: release 19.0.0-rc.0

--- schematics-core/utility/libs-version.js
+++ schematics-core/utility/libs-version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.platformVersion = void 0;
-exports.platformVersion = '^19.0.0-beta.0';
+exports.platformVersion = '^19.0.0-rc.0';
 //# sourceMappingURL=libs-version.js.map
